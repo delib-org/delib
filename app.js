ee38eee9289a710cbead0e327546b4b393506958
Hai Dvash <tald123@gmail.com>
2016-03-22 20:33:21 +0200

    app.js resotred

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-/**
- * Created by kido on 2016/03/21.
- */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/groups', function (req, res) {
+    res.send('Groups List');
+});
+app.get('/users', function (req, res) {
+    res.send('Users List');
+});
+app.get('/options', function (req, res) {
+    res.send('Options List');
+});
+app.get('/questions', function (req, res) {
+    res.send('Hello World!');
+});
+app.get('/issues', function (req, res) {
+    res.send('Issues List');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
