525008b8070edfc7557874d2fbc7e3601f818f31
Hai Dvash <tald123@gmail.com>
2016-04-18 20:45:56 +0300

    ADDED: simple routing skeleton

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
-
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users  = require('./routes/users');
+
 var app = express();
-var router = express.Router();
 
-app.get('/', function (req, res) {
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    res.send('Hello World!');
-});
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/groups', function (req, res) {
-    res.send('Groups List');
-});
-app.get('/users', function (req, res) {
-    res.send('Users List');
-});
-app.get('/options', function (req, res) {
-    res.send('Options List');
-});
-app.get('/questions', function (req, res) {
-    res.send('Hello World!');
-});
-app.get('/issues', function (req, res) {
-    res.send('Issues List');
-});
+app.use('/', routes);
+app.use('/users', users);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
-      res.send('Hello World!');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/groups', function (req, res) {
-      res.send('Groups List');
-});
-app.get('/users', function (req, res) {
-      res.send('Users List');
-});
-app.get('/options', function (req, res) {
-      res.send('Options List');
-});
-app.get('/questions', function (req, res) {
-      res.send('Hello World!');
-});
-app.get('/issues', function (req, res) {
-      res.send('Issues List');
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: (app.get('env') === 'development') ? err : {}
+    });
 });
 
-app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
-});
+
+module.exports = app;
--- run/www.js
+++ run/www.js
@@ -1,14 +1,13 @@
 console.log('hello');
-//var debug = require('debug')('express-example');
-//var app = require('../app');
+var debug = require('debug')('express-example');
+var app = require('../app');
 var models = require("../JavaScript/models");
 
-//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
-models.DBmediator.sync();
-//
-// .then(function () {
-//     var server = app.listen(app.get('port'), function() {
-//         debug('Express server listening on port ' + server.address().port);
-//     });
-// });
+models.DBmediator.sync()
+.then(function () {
+    var server = app.listen(app.get('port'), function() {
+        debug('Express server listening on port ' + server.address().port);
+    });
+});
